57637832c57cf3abad3242aa1c876f45054a1f06
Nella <nella.nieminen@gmail.com>
2022-03-31 20:14:08 +0300

    update ui

--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ class App:
     def run(self):
 
         self.io.write('Welcome to Reading Tip software!\n')
-        self.io.write('Choose from the following operations:\n 1. Add a reading tip\n 2. Exit software\n')
+        self.io.write('Choose from the following operations:\n  1. Add a Reading Tip\n  2. Modify a Reading Tip\n  3. Exit software\n')
 
         while True:
             command = self.io.read("Select the operation you want to run (numbers only): ")
@@ -19,6 +19,13 @@ class App:
                     self.io.write(str(error))
 
             elif command == "2":
+
+                try:
+                    self.reading_tip_service.modify_reading_tip()
+                except Exception as error:
+                    self.io.write(str(error))
+
+            elif command == "3":
                 self.io.write("\nSession ended.\n")
                 break
         
