0d520a4d47f33358233b148dd619fa1d0e943f19
Nella <nella.nieminen@gmail.com>
2022-03-31 19:38:56 +0300

    update ui and services

--- src/app.py
+++ src/app.py
@@ -1,19 +1,28 @@
 class App:
-    def __init__(self, io):
+    def __init__(self, reading_tip_service, io):
         self.io = io
+        self.reading_tip_service = reading_tip_service
     
     def run(self):
 
         self.io.write('Welcome to Reading Tip software!\n')
-        self.io.write("Choose from the following operations:\n 1. Add a reading tip\n 2. Exit software\n")
+        self.io.write('Choose from the following operations:\n 1. Add a reading tip\n 2. Exit software\n')
 
         while True:
             command = self.io.read("Select the operation you want to run (numbers only): ")
             if command == "1":
-                self.io.write("\nlisätään kantaan\n")
+
+                try:
+                    self.reading_tip_service.create_reading_tip()
+                    self.io.write("\nNew Reading Tip added to database.\n")
+                except Exception as error:
+                    self.io.write(str(error))
+
             elif command == "2":
                 self.io.write("\nSession ended.\n")
                 break
+        
+        
 
         
         
---
+++ src/services/reading_tip_service.py
@@ -0,0 +1,24 @@
+from entities.reading_tip import ReadingTip
+
+class ReadingTipService:
+    def __init__(self, reading_tip_repository=None):
+        self._reading_tip_repository = reading_tip_repository
+    
+    def create_reading_tip(self):
+        #self._reading_tip_repository.create(ReadingTip())
+        print("Testataan create_reading_tip")
+        pass
+
+    def remove_reading_tip(self):
+        #self._reading_tip_repository.remove(ReadingTip())
+        print("Testataan remove_reading_tip")
+        pass
+
+    def search_reading_tip(self):
+        pass
+
+
+    
+
+        
+
