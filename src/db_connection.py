caa9ddae21c5e47e2b050aa416192c795aa4f6ce
Sari Lautanala <sari.lautanala@helsinki.fi>
2022-04-04 21:54:14 +0300

    Small changes for test database

--- src/db_connection.py
+++ src/db_connection.py
@@ -4,3 +4,7 @@ db = sqlite3.connect("readingtips.db")
 
 def get_db_connection():
     return db
+
+def get_db_connection_for_testing():
+    db_test = sqlite3.connect("test_readingtips.db")
+    return db_test 
--- src/initialize_db.py
+++ src/initialize_db.py
@@ -1,4 +1,4 @@
-from db_connection import get_db_connection
+from db_connection import get_db_connection, get_db_connection_for_testing
 
 
 def drop_tables_from_db(db_connection):
@@ -34,6 +34,15 @@ def initialize_db():
     """
 
     db_connection = get_db_connection()
+
+    drop_tables_from_db(db_connection)
+    create_tables_to_db(db_connection)
+
+def initialize_db_for_testing():
+    """Drops all tables from test db and creates all the same tables again.
+    """
+
+    db_connection = get_db_connection_for_testing()
 
     drop_tables_from_db(db_connection)
     create_tables_to_db(db_connection)
