018fae3ec71c05e18078c3e616d3ceb5bdc6f9d1
Riikka <74421508+riikkayoki@users.noreply.github.com>
2022-04-07 10:51:40 +0300

    korjaus testit (#57)
    * korjaus sql
    
    * korjaus jotta testit toimii
    
    * testit korjattu ja CI hymyilee
    
    * korjaus update metodi väärä sisennys
    
    * refaktorointia tagit
    
    * robot testit toimii
    
    * rakennetta muokattu
    
    * tag nimi vaihettu
    
    * tag tips taulut tehty
    
    * testit toimii

---
+++ src/entities/tag.py
@@ -0,0 +1,13 @@
+class Tag:
+    def __init__(self):
+        self.tags = []
+        self.tags_in_reading_tip = []
+
+    def create_new_tag(self, tag_name):
+        """Creates new tag to the """
+        if tag_name not in self.tags:
+            self.tags.append(tag_name)
+
+    def add_tag_to_reading_tip(self, tag_name):
+        if tag_name not in self.tags:
+            self.tags_in_reading_tip.append(tag_name)
